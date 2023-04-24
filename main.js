c15cc1f24b62ae8e0e3d11beaea51c5f1d5acad0
Gabriel-Manchego <82614236+Gabriel-Manchego@users.noreply.github.com>
2023-04-24 18:20:45 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function dinamic(){
+    let content = `<div>
+        <p>
+            ${exp.value}
+        </p>
+        <p>
+            ${quantity.value}
+        </p>
+        <p>
+            ${quantity.value}
+        </p>
+        <p>
+            ${quantity.value}
+        </p>
+    </div>`;
+
+    list.innerHTML += content;
+}
